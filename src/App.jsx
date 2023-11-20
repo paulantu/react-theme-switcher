40b6c274015c1b9176a2513b906f1b9ec410c0e9
Antu Paul <antupaul81@gmail.com>
2023-11-20 23:55:46 +0600

    card design with darkmode and lightmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from "react"
+import { ThemeProvider } from "./context/theme"
+import ThemeButton from "./components/ThemeButton";
+import Card from "./components/Card";
 
 
 function App() {
-  
+
+
+
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark");
+
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
+
+
+
   return (
-    <>
-      
-    </>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              {/* toggler section*/}
+              <ThemeButton />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+              {/* cart section */}
+
+              <Card />
+
+          </div>
+      </div>
+    </div>
+    </ThemeProvider>
+    
+
   )
 }
 
